87a79fc36b4560f55192db0bc14ece17938d621f
Raya Straus <raya@cloudinary.com>
2020-12-16 17:28:15 +0200

    added docstrings

--- html/src/accessibility.ts
+++ html/src/accessibility.ts
@@ -2,12 +2,21 @@ import {CloudinaryImage} from "@cloudinary/base/assets/CloudinaryImage";
 import {plugin, accessibilityMode} from "./types";
 import {ACCESSIBILITY_MODES} from './constants';
 
+/**
+ * Returns the accessibility plugin
+ * @param mode 'darkmode' | 'brightmode' | 'monochrome' | 'colorblind'
+ */
 export function accessibility(mode='darkmode'): plugin{
   return accessibilityPlugin.bind(null, mode);
 }
 
-
-
+/**
+ * Accessibility transformations are appended to the original image.
+ * @param mode Accessbility mode 'darkmode' | 'brightmode' | 'monochrome' | 'colorblind'
+ * @param element The image element
+ * @param cloudinaryImage
+ * @param runningPlugins holds running plugins to be canceled
+ */
 export function accessibilityPlugin(mode?: accessibilityMode, element?: HTMLImageElement, cloudinaryImage?: CloudinaryImage, runningPlugins?: Function[]): Promise<void | string> | string {
   return new Promise((resolve) => {
     runningPlugins.push(()=>{
