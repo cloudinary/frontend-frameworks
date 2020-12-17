f76ac044a598e5828d94fe690e5f4890fe8cbcff
Raya Straus <raya@cloudinary.com>
2020-12-17 16:56:46 +0200

    implemented responsive

--- html/src/responsive.ts
+++ html/src/responsive.ts
@@ -1,15 +1,51 @@
-// import * as Resize from '@cloudinary/base/actions/resize/Resize';
-import {isBrowser} from "./isBrowser";
 import {CloudinaryImage} from "@cloudinary/base/assets/CloudinaryImage";
+import {plugin} from "./types";
+import {scale} from "@cloudinary/base/actions/resize";
+import debounce from 'lodash.debounce';
 
-export function responsive(element: any, transformableImage: CloudinaryImage, toBeCanceled: any): Promise<void | string> | string {
-  if(!isBrowser()){
-    return;
-  }
-  return new Promise((resolve)=>{
-    const size = element.parentElement.clientWidth;
-    // transformableImage.resize(Resize.scale(size))
-    console.log('responsive done');
-    resolve();
-  })
+/**
+ * Returns the responsive plugin
+ */
+export function responsive(steps?: number | number[]): plugin{
+  return responsivePlugin.bind(null, steps);
 }
+
+function responsivePlugin(steps?: number | number[], element?:HTMLImageElement, cloudinaryImage?: CloudinaryImage, runningPlugins?: Function[]): Promise<void | string> | string {
+  return new Promise((resolve)=>{
+    runningPlugins.push(()=>{
+      //window.removeEventListener('resize', resizeEvent);
+      resolve('canceled');
+    });
+    const containerSize = element.parentElement.clientWidth;
+    //cloudinaryImage.resize(scale().width(containerSize).setActionTag('responsive'));
+
+    cloudinaryImage.resize(scale().width(containerSize));
+
+
+    window.addEventListener('resize', debounce(()=>{
+      let resizeValue = element.parentElement.clientWidth;
+
+      if(typeof steps === 'number'){
+        resizeValue = Math.ceil(resizeValue/steps)*steps;
+      }
+
+      if(typeof steps === 'object'){
+
+      }
+
+      element.src = cloudinaryImage.resize(scale().width(resizeValue)).toURL();
+
+    }, 100));
+
+    resolve();
+  });
+}
+
+
+
+/*
+  step 100px
+
+    60 then round 100
+    150 round 200
+ */
