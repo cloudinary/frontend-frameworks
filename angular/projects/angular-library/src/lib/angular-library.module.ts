94fee232f2d81326e6bc18e7aa17e154fce5ddde
Raya Straus <raya@cloudinary.com>
2020-12-29 15:23:50 +0200

    remove unused imports

--- angular/projects/angular-library/src/lib/angular-library.module.ts
+++ angular/projects/angular-library/src/lib/angular-library.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
-import { AngularLibraryComponent } from './angular-library.component';
 import { CloudinaryImageComponent } from './cloudinary-image.component';
 
 @NgModule({
   imports: [
   ],
-  declarations: [AngularLibraryComponent, CloudinaryImageComponent],
-  exports: [AngularLibraryComponent, CloudinaryImageComponent]
+  declarations: [CloudinaryImageComponent],
+  exports: [CloudinaryImageComponent]
 })
 export class AngularLibraryModule { }
