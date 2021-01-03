1fd58d9e77dcb4f534edda095a438a7a663bd49c
Patrick <patrick.tolosa@cloudinary.com>
2021-01-03 12:53:57 +0200

    Fix pre-release script

--- packages/html/preRelease.sh
+++ packages/html/preRelease.sh
@@ -1,13 +1,10 @@
-PACKAGE_VERSION=$(cat package.json | grep \\\"version\\\" | head -1 | awk -F: '{ print $2 }' | sed 's/[\",]//g' | tr -d '[[:space:]]');
-
-echo $PACKAGE_VERSION
 
 # Bump all versions
-npm version prerelease
-npm version prerelease --prefix ./html
-npm version prerelease --prefix ./react
-npm version prerelease --prefix ./angular
-npm version prerelease --prefix ./angular/projects/angular-library
+npm version prerelease --no-git-tag-version
+npm version prerelease --no-git-tag-version --prefix ./html
+npm version prerelease --no-git-tag-version --prefix ./react
+npm version prerelease --no-git-tag-version --prefix ./angular
+npm version prerelease --no-git-tag-version --prefix ./angular/projects/angular-library
 
 # Build all libraries
 npm run build --prefix ./html
@@ -15,10 +12,12 @@ npm run build --prefix ./react
 npm run build --prefix ./angular
 
 # Release all libraries
-cd ./html/dist && npm run release && cd ../../
-cd ./react/dist && npm run release && cd ../../
-cd ./angular/dist/angular-library && npm run release && cd ../../../
+cd ./html/dist && npm publish --access public && cd ../../
+cd ./react/dist && npm publish --access public && cd ../../
+cd ./angular/dist/angular-library && npm publish --access public && cd ../../../
+
 
 # Add all new package.json's changes to git
+PACKAGE_VERSION=$(cat package.json | grep \\\"version\\\" | head -1 | awk -F: '{ print $2 }' | sed 's/[\",]//g' | tr -d '[[:space:]]');
 git commit -am "New prerelease $PACKAGE_VERSION"
 git tag "${PACKAGE_VERSION}" -am "Version ${PACKAGE_VERSION}"
