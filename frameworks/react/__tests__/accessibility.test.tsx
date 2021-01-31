0e6253608f14f06670cd4e214d71815bfd770356
Raya Straus <raya@cloudinary.com>
2021-01-31 11:59:36 +0200

    add linitin

--- frameworks/react/__tests__/accessibility.test.tsx
+++ frameworks/react/__tests__/accessibility.test.tsx
@@ -1,60 +1,81 @@
 import { AdvancedImage, accessibility } from '../src'
-import {CloudinaryImage} from "@cloudinary/base/assets/CloudinaryImage";
-import {mount} from 'enzyme';
-import React  from "react";
+import { CloudinaryImage } from '@cloudinary/base/assets/CloudinaryImage';
+import { mount } from 'enzyme';
+import React from 'react';
 
-const cloudinaryImage = new CloudinaryImage('sample', { cloudName: 'demo'});
+const cloudinaryImage = new CloudinaryImage('sample', { cloudName: 'demo' });
 
 describe('accessibility', () => {
-  it("should apply default",  function (done) {
-    let component = mount(<AdvancedImage cldImg={cloudinaryImage} plugins={[accessibility()]}/>);
-    setTimeout(()=>{
-      expect(component.html()).toBe("<img src=\"https://res.cloudinary.com/demo/image/upload/co_black,e_colorize:70/sample\">");
+  it('should apply default', function (done) {
+    const component = mount(<AdvancedImage cldImg={cloudinaryImage} plugins={[accessibility()]} />);
+    setTimeout(() => {
+      expect(component.html()).toBe('<img src="https://res.cloudinary.com/demo/image/upload/co_black,e_colorize:70/sample">');
       done();
     }, 0);// one tick
   });
 
-  it("should apply darkmode",  function () {
-    let component = mount(<AdvancedImage cldImg={cloudinaryImage} plugins={[accessibility('darkmode')]}/>);
-    setTimeout(()=>{
-      expect(component.html()).toBe("<img src=\"https://res.cloudinary.com/demo/image/upload/co_black,e_colorize:70/sample\">");
+  it('should apply darkmode', function () {
+    const component = mount(<AdvancedImage cldImg={cloudinaryImage} plugins={[accessibility('darkmode')]} />);
+    setTimeout(() => {
+      expect(component.html()).toBe('<img src="https://res.cloudinary.com/demo/image/upload/co_black,e_colorize:70/sample">');
     }, 0);// one tick
   });
 
-  it("should apply brightmode",  function (done) {
-    let component = mount(<AdvancedImage cldImg={cloudinaryImage} plugins={[accessibility('brightmode')]}/>);
-    setTimeout(()=>{
-      expect(component.html()).toBe("<img" +
-        " src=\"https://res.cloudinary.com/demo/image/upload/co_white,e_colorize:40/sample\">");
+  it('should apply brightmode', function (done) {
+    const component = mount(<AdvancedImage cldImg={cloudinaryImage} plugins={[accessibility('brightmode')]} />);
+    setTimeout(() => {
+      expect(component.html()).toBe('<img' +
+        ' src="https://res.cloudinary.com/demo/image/upload/co_white,e_colorize:40/sample">');
       done();
     }, 0);// one tick
   });
 
-  it("should apply monochrome",  function (done) {
-    let component = mount(<AdvancedImage cldImg={cloudinaryImage} plugins={[accessibility('monochrome')]}/>);
-    setTimeout(()=>{
-      expect(component.html()).toBe("<img" +
-        " src=\"https://res.cloudinary.com/demo/image/upload/e_grayscale/sample\">");
+  it('should apply monochrome', function (done) {
+    const component = mount(<AdvancedImage cldImg={cloudinaryImage} plugins={[accessibility('monochrome')]} />);
+    setTimeout(() => {
+      expect(component.html()).toBe('<img' +
+        ' src="https://res.cloudinary.com/demo/image/upload/e_grayscale/sample">');
       done();
     }, 0);// one tick
   });
 
-  it("should apply colorblind",  function (done) {
-    let component = mount(<AdvancedImage cldImg={cloudinaryImage} plugins={[accessibility('colorblind')]}/>);
-    setTimeout(()=>{
-      expect(component.html()).toBe("<img" +
-        " src=\"https://res.cloudinary.com/demo/image/upload/e_assist_colorblind/sample\">");
+  it('should apply colorblind', function (done) {
+    const component = mount(<AdvancedImage cldImg={cloudinaryImage} plugins={[accessibility('colorblind')]} />);
+    setTimeout(() => {
+      expect(component.html()).toBe('<img' +
+        ' src="https://res.cloudinary.com/demo/image/upload/e_assist_colorblind/sample">');
       done();
     }, 0);// one tick
   });
 
-  it("should default if supplied with incorrect mode",  function (done) {
-    let component = mount(<AdvancedImage cldImg={cloudinaryImage} plugins={[accessibility('ddd')]}/>);
-    setTimeout(()=>{
-      expect(component.html()).toBe("<img" +
-        " src=\"https://res.cloudinary.com/demo/image/upload/co_black,e_colorize:70/sample\">");
+  it('should default if supplied with incorrect mode', function (done) {
+    const component = mount(<AdvancedImage cldImg={cloudinaryImage} plugins={[accessibility('ddd')]} />);
+    setTimeout(() => {
+      expect(component.html()).toBe('<img' +
+        ' src="https://res.cloudinary.com/demo/image/upload/co_black,e_colorize:70/sample">');
       done();
     }, 0);// one tick
   });
 
+  it('should update without errors', function (done) {
+    const component = mount(<AdvancedImage cldImg={cloudinaryImage} plugins={[accessibility()]} />);
+
+    component.update();
+
+    setTimeout(() => {
+      expect(component.html()).toBe('<img src="https://res.cloudinary.com/demo/image/upload/co_black,e_colorize:70/sample">');
+      done();
+    }, 0);// one tick
+  });
+
+  it('should unmount', function (done) {
+    const component = mount(<AdvancedImage cldImg={cloudinaryImage} plugins={[accessibility()]} />);
+
+    component.unmount();
+
+    setTimeout(() => {
+      expect(component.html()).toBe('<img src="https://res.cloudinary.com/demo/image/upload/co_black,e_colorize:70/sample">');
+      done();
+    }, 0);// one tick
+  });
 });
