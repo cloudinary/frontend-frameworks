1e04e184ad63ab5b3c8e045349aab9b2b743570b
Patrick <patrick.tolosa@cloudinary.com>
2021-01-31 13:25:44 +0200

    finalize-scripts-for-release

---
+++ scripts/setup.sh
@@ -0,0 +1,16 @@
+rm -rf node_modules
+rm -rf frameworks/react/node_modules
+rm -rf frameworks/html/node_modules
+rm -rf frameworks/angular/node_modules
+
+npm install --prefix frameworks/html #install html
+npm run build --prefix frameworks/html #install html
+npm install . #install root, which also links html/dist
+
+npm install --force --prefix frameworks/react
+npm install --force --prefix frameworks/angular
+
+rm -rf frameworks/react/node_modules/@cloudinary/html
+
+npm run build --prefix frameworks/react
+npm run build --prefix frameworks/angular
