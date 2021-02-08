80c365d96c902fc7866febe13de50af3357fc483
patrick-tolosa <59408474+patrick-tolosa@users.noreply.github.com>
2021-02-08 15:21:56 +0200

    Docs - Finalize readme before release (#45)
    * Remove .idea project file
    * Docs - Finalize readme before release
    * Update readme title and fix a broken example of plugins

--- packages/html/scripts/buildDocs.js
+++ packages/html/scripts/buildDocs.js
@@ -2,4 +2,4 @@
 const { execSync } = require('child_process');
 const fs = require('fs');
 
-execSync(`jsdoc  --configure jsdoc.config.json --verbose --readme ./DOCS.README.md --destination public/docs/ && cp __DOC_RESOURCES__/customStyles.css public/docs/`, {stdio: 'inherit'})
+execSync(`jsdoc  --configure jsdoc.config.json --verbose --destination public/docs/ && cp __DOC_RESOURCES__/customStyles.css public/docs/`, {stdio: 'inherit'})
