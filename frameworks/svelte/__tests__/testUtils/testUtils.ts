6e87033738ca7e8bc3e53b27d3aa3f7d985a88de
maoznir <nir.maoz@cloudinary.com>
2021-02-09 18:51:58 +0200

    Update svelte sdk according to pr review

--- frameworks/svelte/__tests__/testUtils/testUtils.ts
+++ frameworks/svelte/__tests__/testUtils/testUtils.ts
@@ -1,15 +1,16 @@
-import {SvelteComponent, tick} from "svelte";
+import {tick} from "svelte";
 import {render, RenderResult} from '@testing-library/svelte'
 import {testWithMockedIntersectionObserver} from '../../../../testUtils/setupIntersectionObserverMock';
+import type {SvelteComponentDev} from "svelte/internal";
 
-const getImageElement = (container: HTMLElement): HTMLImageElement => container.querySelector("img");
+const getImageElement = (container: HTMLElement): HTMLImageElement => container.querySelector("img") || {} as HTMLImageElement;
 
-const getImageAttr = (container: HTMLElement, attr: string): any => (getImageElement(container) || {})[attr];
+const getImageAttr = (container: HTMLElement, attr: string): any => ((getImageElement(container) || {}) as any)[attr];
 
-const mount = async (cmp?: typeof SvelteComponent, props?: any): Promise<RenderResult> => {
-  const result = render(cmp, props);
+const mount = async (cmp?: typeof SvelteComponentDev, props?: any): Promise<RenderResult> => {
+  const result = cmp ? render(cmp, props) : {};
   await tick();
-  return result;
+  return result as RenderResult;
 };
 
 export {testWithMockedIntersectionObserver, getImageElement, getImageAttr, mount};
