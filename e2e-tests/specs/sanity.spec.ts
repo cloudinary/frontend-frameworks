1e6f837dd465d41d60d3325752759c09e5fa75b2
maoznir <nir.maoz@cloudinary.com>
2021-03-11 08:23:36 +0200

    Remove unneeded files

--- e2e-tests/specs/sanity.spec.ts
+++
@@ -1,9 +0,0 @@
-import { expect } from 'chai';
-import { describeCommon } from '../TestHelper';
-
-describeCommon('Sanity', () => {
-  it('Sanity test', () => {
-    console.log('sanity');
-    expect(true).to.equal(true);
-  });
-});
