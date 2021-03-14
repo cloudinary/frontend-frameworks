1cba7c5652b3653b4f7786a158cb76202de48f98
maoznir <nir.maoz@cloudinary.com>
2021-03-15 00:32:59 +0200

    Add responsive unit tests for svelte sdk

---
+++ frameworks/svelte/__tests__/testUtils/ResponsiveHelperWrapper.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  // Helper wrapper for responsive tests
+
+  import {AdvancedImage} from '../../src/index';
+  import {CloudinaryImage} from '@cloudinary/base/assets/CloudinaryImage';
+  import {onMount} from "svelte";
+
+  const cloudinaryImage = new CloudinaryImage('sample', {cloudName: 'demo'});
+  export let width: number = 250; //update container width
+  export let cldImg = undefined; //set child elements
+  export let advancedImgProps = {}; //set child element props
+  let div;
+
+  $: style = `width: ${width};`;
+
+  onMount(()=>{
+    Object.defineProperty(div, 'clientWidth', {value: 250, configurable: true});
+  });
+</script>
+
+<div bind:this={div} id="wrapper">
+    {#if cldImg}
+        <AdvancedImage cldImg={cldImg} {...advancedImgProps} />
+    {:else}
+        <slot />
+    {/if}
+</div>
--- frameworks/svelte/__tests__/testUtils/testUtils.ts
+++ frameworks/svelte/__tests__/testUtils/testUtils.ts
@@ -3,9 +3,13 @@ import {render, RenderResult} from '@testing-library/svelte'
 import {testWithMockedIntersectionObserver} from '../../../../testUtils/setupIntersectionObserverMock';
 import type {SvelteComponentDev} from "svelte/internal";
 
-const getImageElement = (container: HTMLElement): HTMLImageElement => container.querySelector("img") || {} as HTMLImageElement;
+const getElement = (container: HTMLElement, type: string): HTMLElement => container.querySelector(type) || {} as HTMLElement;
+
+const getImageElement = (container: HTMLElement): HTMLImageElement => getElement(container, 'img') as HTMLImageElement;
 
 const getImageAttr = (container: HTMLElement, attr: string): any => ((getImageElement(container) || {}) as any)[attr];
+
+const getElementAttr = (container: HTMLElement, type: string, attr: string): any => ((getElement(container, type) || {}) as any)[attr];
 
 const mount = async (cmp?: typeof SvelteComponentDev, props?: any): Promise<RenderResult> => {
   const result = cmp ? render(cmp, props) : {};
@@ -13,4 +17,43 @@ const mount = async (cmp?: typeof SvelteComponentDev, props?: any): Promise<Rend
   return result as RenderResult;
 };
 
-export {testWithMockedIntersectionObserver, getImageElement, getImageAttr, mount};
+/**
+ * Resize given element and dispatch a window resize event
+ * @param element to resize
+ * @param width to set element's clientWidth to
+ * @return the modified element
+ */
+const resizeElement = (element?: any, width?: number) => {
+  if (element) {
+    Object.defineProperty(element, 'clientWidth', {value: width, configurable: true});
+  }
+  window.dispatchEvent(new Event('resize'));
+
+  return element;
+}
+
+/**
+ * Util function used to dispatch a resize event used in the responsive tests
+ * Resize #wrapper in given svelte container to given width and dispatch a window resize event.
+ * @param container
+ * @param width
+ * @return the modified element
+ */
+const dispatchResize = (container?: any, width?: number) => {
+  if (container) {
+    return resizeElement(getElement(container, '#wrapper'), width);
+  }
+
+  return resizeElement();
+}
+
+
+export {
+  testWithMockedIntersectionObserver,
+  getImageElement,
+  getImageAttr,
+  getElement,
+  getElementAttr,
+  mount,
+  dispatchResize
+};
