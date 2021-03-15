cbdcbfff24740e89462bb5f3516787c049d24967
Nir Maoz <47939921+maoznir@users.noreply.github.com>
2021-03-15 11:46:43 +0200

    Add responsive unit tests for svelte sdk (#60)

---
+++ frameworks/svelte/__tests__/responsive.test.ts
@@ -0,0 +1,108 @@
+import waitForExpect from 'wait-for-expect';
+import { crop } from '@cloudinary/base/actions/resize';
+import {CloudinaryImage} from '@cloudinary/base/assets/CloudinaryImage';
+import {responsive} from '../src/index';
+import {default as MockWrapper} from './testUtils/ResponsiveHelperWrapper.svelte';
+import {getImageAttr, mount, getElement, dispatchResize} from "./testUtils/testUtils";
+
+const cloudinaryImage = new CloudinaryImage('sample', {cloudName: 'demo'});
+
+describe('responsive', () => {
+  it('should apply initial container width (default 250)', async () => {
+    const {container} = await mount(MockWrapper, {
+      cldImg: cloudinaryImage,
+      advancedImgProps: {plugins: [responsive()]}
+    });
+
+    const el = getElement(container, '#wrapper');
+    await waitForExpect(() => {
+      expect(el.clientWidth).toBe(250);
+    });
+  });
+
+  it('should update container width on window resize', async () => {
+    const {container} = await mount(MockWrapper, {
+      cldImg: cloudinaryImage,
+      advancedImgProps: {plugins: [responsive()]}
+    });
+
+    let el = dispatchResize(container, 100);
+    await waitForExpect(() => {
+      expect(el.clientWidth).toBe(100);
+    });
+  });
+
+  it('should step by the 100th', async function () {
+    const {container} = await mount(MockWrapper, {
+      cldImg: cloudinaryImage,
+      advancedImgProps: {plugins: [responsive(100)]}
+    });
+
+    dispatchResize();
+
+    await waitForExpect(() => {
+      expect(getImageAttr(container, 'src')).toBe('https://res.cloudinary.com/demo/image/upload/c_scale,w_300/sample');
+    });
+  });
+
+  it('should step by breakpoints', async function () {
+    const {container} = await mount(MockWrapper, {
+      cldImg: cloudinaryImage,
+      advancedImgProps: {plugins: [responsive([800, 1000, 1200, 3000])]}
+    });
+
+    dispatchResize();
+
+    await waitForExpect(() => {
+      expect(getImageAttr(container, 'src')).toBe('https://res.cloudinary.com/demo/image/upload/c_scale,w_800/sample');
+    });
+
+    // simulate resize to 975
+    dispatchResize(container, 975);
+
+    await waitForExpect(() => {
+      expect(getImageAttr(container, 'src')).toBe('https://res.cloudinary.com/demo/image/upload/c_scale,w_1000/sample');
+    });
+  });
+
+  it('should not resize to larger than provided breakpoints', async function () {
+    const {container} = await mount(MockWrapper, {
+      cldImg: cloudinaryImage,
+      advancedImgProps: {plugins: [responsive([800, 1000, 1200, 3000])]}
+    });
+
+    dispatchResize(container, 4000);
+
+    await waitForExpect(() => {
+      expect(getImageAttr(container, 'src')).toBe('https://res.cloudinary.com/demo/image/upload/c_scale,w_3000/sample');
+    });
+  });
+
+  it('should handle unordered breakpoints', async function () {
+    const {container} = await mount(MockWrapper, {
+      cldImg: cloudinaryImage,
+      advancedImgProps: {plugins: [responsive([1000, 800, 3000, 1200])]}
+    });
+
+    dispatchResize(container, 5000);
+
+    await waitForExpect(() => {
+      expect(getImageAttr(container, 'src')).toBe('https://res.cloudinary.com/demo/image/upload/c_scale,w_3000/sample');
+    });
+  });
+
+  it('should append to existing transformation', async function () {
+    cloudinaryImage.resize(crop('500'));
+
+    const {container} = await mount(MockWrapper, {
+      cldImg: cloudinaryImage,
+      advancedImgProps: {plugins: [responsive()]}
+    });
+
+    dispatchResize();
+
+    await waitForExpect(() => {
+      expect(getImageAttr(container, 'src')).toBe('https://res.cloudinary.com/demo/image/upload/c_crop,w_500/c_scale,w_250/sample');
+    });
+  });
+});
