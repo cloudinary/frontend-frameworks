edb7f602ca39a75e0b2744ef950bfbd2af65e55c
maoznir <nir.maoz@cloudinary.com>
2021-04-17 20:28:56 +0300

    Add Vue 3 sdk infrastructure

---
+++ frameworks/vue/vue3/src/components/AdvancedImage.vue
@@ -0,0 +1,14 @@
+<template>
+  <img src="https://res.cloudinary.com/demo/image/upload/sample" alt="">
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'AdvancedImage',
+  props: {
+    cldImage: Object,
+  },
+});
+</script>
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,6 +2,8 @@ rm -rf node_modules
 rm -rf frameworks/react/node_modules
 rm -rf frameworks/html/node_modules
 rm -rf frameworks/angular/node_modules
+rm -rf frameworks/vue/vue3/node_modules
+rm -rf frameworks/svelte/node_modules
 rm -rf e2e-tests/node_modules
 
 npm install --prefix frameworks/html #install html
@@ -10,12 +12,15 @@ npm install . #install root, which also links html/dist
 
 npm install --force --prefix frameworks/react
 npm install --force --prefix frameworks/angular
+npm install --force --prefix frameworks/vue/vue3
 npm install --force --prefix frameworks/svelte
 npm install --force --prefix e2e-tests
 
 rm -rf frameworks/react/node_modules/@cloudinary/html
+rm -rf frameworks/vue/vue3/node_modules/@cloudinary/html
 rm -rf frameworks/svelte/node_modules/@cloudinary/html
 
 npm run build --prefix frameworks/react
 npm run build --prefix frameworks/angular
+npm run build --prefix frameworks/vue/vue3
 npm run build --prefix frameworks/svelte
