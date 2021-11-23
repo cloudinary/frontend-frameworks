b3b35f1c18fd7b2f7bfc0fb40084757febd041a7
strausr <Raya@cloudinary.com>
2021-11-23 11:26:54 +0200

    Feature/add lerna (#104)

--- frameworks/vue/vue3/src/index.ts
+++
@@ -1,7 +0,0 @@
-import AdvancedImage from './components/AdvancedImage.vue';
-import AdvancedVideo from './components/AdvancedVideo.vue';
-
-export {
-  AdvancedImage,
-  AdvancedVideo
-};
--- scripts/test.sh
+++
@@ -1,9 +0,0 @@
-(cd ./frameworks/react && npm run test) &&
-(cd ./frameworks/angular && npm run test) &&
-(cd ./frameworks/vue/vue3 && npm run test) &&
-(cd ./frameworks/svelte && npm run test)
-# Uncomment this when e2e tests are stable (and webdriver stops throwing errors)
-#(cd ./e2e-tests && npm run start-server-and-test:react)
-# Uncomment this when Vue sdk is expected to pass e2e tests
-# (cd ./e2e-tests && npm run start-server-and-test:vue)
-
