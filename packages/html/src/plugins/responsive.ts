3773ecfa7c52b61c7822593e65cf61672c140959
strausr <Raya@cloudinary.com>
2022-02-07 12:43:30 +0200

    Update packages/html/src/plugins/responsive.ts
    Co-authored-by: carlevison <54800761+carlevison@users.noreply.github.com>

--- packages/html/src/plugins/responsive.ts
+++ packages/html/src/plugins/responsive.ts
@@ -11,7 +11,7 @@ import {isImage} from "../utils/isImage";
  * @namespace
  * @description Updates the src with the size of the parent element and triggers a resize event for
  * subsequent resizing.
- * @param steps {number | number[]} The step size in pixels.
+ * @param steps {number | number[]} The step size in pixels or an array of image widths in pixels.
  * @return {Plugin}
  * @example <caption>NOTE: The following is in React. For further examples, see the Packages tab.</caption>
  * <AdvancedImage cldImg={img} plugins={[responsive({steps: [800, 1000, 1400]})]} />
