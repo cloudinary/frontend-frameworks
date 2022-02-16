c62f7bd78b09b9a9f44158a36b4ca7a322894b19
Patrick Tolosa <patrick.tolosa@cloudinary.com>
2022-02-16 19:18:36 +0200

    Adjust package.json fields

--- packages/react/rollup.config.js
+++ packages/react/rollup.config.js
@@ -11,7 +11,7 @@ export default [
       {
         file: 'dist/index.umd.js',
         format: 'umd',
-        name: 'CloudinaryBaseSDK'
+        name: 'CloudinaryReactSDK'
       },
       {
         file: 'dist/index.js',
