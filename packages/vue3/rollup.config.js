52d89b7e7f0671b0f75fa23f9667fd7b37555055
nir maoz <nir.maoz@cloudinary.com>
2022-07-14 17:20:46 +0300

    chore: Remove unneeded comment

--- packages/vue3/rollup.config.js
+++ packages/vue3/rollup.config.js
@@ -1,33 +1,6 @@
 import typescript from "rollup-plugin-typescript2";
 import vue from "rollup-plugin-vue";
 
-/*
-export default async function config(args) {
-  return {
-    input: "src/index.ts",
-    output: {
-      dir: "dist",
-      format: "cjs",
-      sourcemap: true,
-    },
-    plugins: [
-      vue(),
-      typescript({
-        tsconfigOverride: {
-          compilerOptions: {
-            declaration: true,
-          },
-          include: null,
-        },
-      }),
-      clear({
-        targets: ["./dist"],
-      }),
-    ],
-  };
-}
- */
-
 export default [
   // ESM build to be used with webpack/rollup.
   {
