e89ee887b214e0cf010b3c786952fd361036ae4e
nir maoz <nir.maoz@cloudinary.com>
2022-07-25 13:38:35 +0300

    test: add vue3 lazyload tests

---
+++ packages/vue3/tests/unit/lazyload.spec.ts
@@ -0,0 +1,77 @@
+import { AdvancedImage, lazyload } from "../../src/index";
+import { CloudinaryImage } from "@cloudinary/url-gen/assets/CloudinaryImage";
+import { mount } from "@vue/test-utils";
+import { waitTicks, testWithMockedIntersectionObserver } from "./utils";
+
+const cloudinaryImage = new CloudinaryImage(
+  "sample",
+  { cloudName: "demo" },
+  { analytics: false }
+);
+
+describe("lazy-load", () => {
+  it("should not have src pre-scroll", async () => {
+    const rootDiv = document.createElement("div");
+    rootDiv.id = "root";
+    const div = document.createElement("div");
+    div.style.setProperty("height", "10000px");
+    rootDiv.appendChild(div);
+    document.body.appendChild(rootDiv);
+
+    testWithMockedIntersectionObserver(
+      async (mockIntersectionEvent: ({}) => void) => {
+        const component = mount(AdvancedImage, {
+          props: { cldImg: cloudinaryImage, plugins: [lazyload()] },
+          attachTo: "#root",
+        });
+        mockIntersectionEvent([
+          { isIntersecting: false, target: component.element },
+        ]);
+        await waitTicks(2);
+        // no src pre scroll
+        expect(component.html()).toBe("<img>");
+        expect(component.html()).not.toContain(
+          'src="https://res.cloudinary.com/demo/image/upload/sample"'
+        );
+      }
+    );
+  });
+
+  it("should have src when in view", async () => {
+    testWithMockedIntersectionObserver(
+      async (mockIntersectionEvent: ({}) => void) => {
+        const component = mount(AdvancedImage, {
+          props: { cldImg: cloudinaryImage, plugins: [lazyload()] },
+        });
+        mockIntersectionEvent([
+          { isIntersecting: true, target: component.element },
+        ]);
+        await waitTicks(2);
+
+        expect(component.html()).toContain(
+          'src="https://res.cloudinary.com/demo/image/upload/sample"'
+        );
+      }
+    );
+  });
+
+  it("should set lazyload root margin and threshold", async () => {
+    testWithMockedIntersectionObserver(
+      async (mockIntersectionEvent: ({}) => void) => {
+        const component = mount(AdvancedImage, {
+          props: {
+            cldImg: cloudinaryImage,
+            plugins: [lazyload({ rootMargin: "10px", threshold: 0.5 })],
+          },
+        });
+        mockIntersectionEvent([
+          { isIntersecting: true, target: component.element },
+        ]);
+        await waitTicks(2);
+        expect(component.html()).toContain(
+          'src="https://res.cloudinary.com/demo/image/upload/sample"'
+        );
+      }
+    );
+  });
+});
