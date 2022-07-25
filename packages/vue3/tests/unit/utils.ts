3709888570c0a3cd729b6415381d4124c19f49e0
nir maoz <nir.maoz@cloudinary.com>
2022-07-25 15:12:39 +0300

    Remove unneeded test utils function

--- packages/vue3/tests/unit/utils.ts
+++ packages/vue3/tests/unit/utils.ts
@@ -11,36 +11,3 @@ export const waitTicks = async (ticks: number) => {
     await nextTick();
   }
 };
-
-export const testWithMockedIntersectionObserver = (
-  cb: (...args: any) => void
-) => {
-  let intersectCallback: (
-    root?: Element,
-    rootMargin?: string,
-    thresholds?: number
-  ) => void = () => {}; // will be
-  // populated later
-  const nativeIntersectionObserver = global.IntersectionObserver;
-
-  // Mock IntersectionObserver
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
-  global.IntersectionObserver = class {
-    constructor(
-      cb: (root?: Element, rootMargin?: string, thresholds?: number) => any
-    ) {
-      // This is the callback that notifies when an intersection occurs
-      // We'll store it to use it later
-      intersectCallback = cb;
-    }
-    observe() {}
-    unobserve() {}
-  };
-  cb((root?: Element, rootMargin?: string, thresholds?: number) => {
-    intersectCallback(root, rootMargin, thresholds);
-  });
-
-  // restore
-  global.IntersectionObserver = nativeIntersectionObserver;
-};
