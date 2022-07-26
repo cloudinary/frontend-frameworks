2aa5ded428f1f7469540eb6e025b2bca722e06ca
Nir Maoz <47939921+maoznir@users.noreply.github.com>
2022-07-26 13:41:05 +0300

    test: add vue3 responsive tests (#160)

---
+++ testUtils/dispatchResize.ts
@@ -0,0 +1,11 @@
+/**
+ * Util function used to dispatch a resize event used in the responsive tests
+ * @param component
+ * @param resizeValue
+ */
+export function dispatchResize(component: HTMLDivElement, resizeValue:number){
+  Object.defineProperty(component, 'clientWidth', {value: resizeValue, configurable: true});
+  window.dispatchEvent(new Event('resize'));
+
+  return component;
+}
