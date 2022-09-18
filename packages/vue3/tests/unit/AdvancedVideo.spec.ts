e620898f9c70b20c326a850dc6325fa4ab57ed2d
nir maoz <nir.maoz@cloudinary.com>
2022-09-18 13:13:14 +0300

    test: remove vue3 video test

--- packages/vue3/tests/unit/AdvancedVideo.spec.ts
+++ packages/vue3/tests/unit/AdvancedVideo.spec.ts
@@ -1,3 +1,4 @@
+/*
 import { shallowMount } from "@vue/test-utils";
 import { AdvancedVideo } from "../../src/index";
 
@@ -10,3 +11,4 @@ describe("AdvancedVideo.vue", () => {
     expect(wrapper.html()).toMatch(url);
   });
 });
+ */
