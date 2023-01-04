cbafb47890ed7e66e26bf91e4164e0cb8febe902
Paweł Philipczyk <pawel.philipczyk@cloudinary.com>
2023-01-04 08:07:00 +0100

    chore: SNI-6952 add react advanced video to playground

--- packages/react/playground/src/App.tsx
+++ packages/react/playground/src/App.tsx
@@ -2,19 +2,36 @@ import React from 'react'
 import './App.css'
 import {
   AdvancedImage,
+  AdvancedVideo,
   accessibility,
   responsive,
   lazyload,
   placeholder
 } from '@cloudinary/react/src'
 import { CloudinaryImage } from '@cloudinary/url-gen/assets/CloudinaryImage'
+import { CloudinaryVideo } from '@cloudinary/url-gen/assets/CloudinaryVideo'
 
 // This is our playground and can be used to test library
 function App() {
   const img = new CloudinaryImage('sample', { cloudName: 'demo' })
 
+  const cldVid = new CloudinaryVideo(
+    'docs/walking_talking',
+    { cloudName: 'demo' },
+    { analytics: false }
+  )
+
   return (
     <div>
+      <h1>AdvancedVideo</h1>
+      <AdvancedVideo
+        cldVid={cldVid}
+        controls
+        loop
+        width={600}
+      />
+
+      <h1>AdvancedImage</h1>
       <AdvancedImage cldImg={img} plugins={[responsive({ steps: 100 })]} />
     </div>
   )
