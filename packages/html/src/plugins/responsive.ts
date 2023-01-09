13849942f0d206317d44db0d93846520da1aee1d
Lior Ben-David <lior.ben-david@cloudinary.com>
2023-01-09 14:40:34 +0200

    chore: reformat responsive.ts

--- packages/html/src/plugins/responsive.ts
+++ packages/html/src/plugins/responsive.ts
@@ -17,7 +17,7 @@ import {getAnalyticsOptions} from "../utils/analytics";
  * @example <caption>NOTE: The following is in React. For further examples, see the Packages tab.</caption>
  * <AdvancedImage cldImg={img} plugins={[responsive({steps: [800, 1000, 1400]})]} />
  */
-export function responsive({steps}: { steps?: number | number[] } = {}): Plugin {
+export function responsive({steps}:{steps?: number | number[]}={}): Plugin{
     return responsivePlugin.bind(null, steps);
 }
 
@@ -29,14 +29,14 @@ export function responsive({steps}: { steps?: number | number[] } = {}): Plugin 
  * @param htmlPluginState {HtmlPluginState} holds cleanup callbacks and event subscriptions
  * @param analyticsOptions {AnalyticsOptions} analytics options for the url to be created
  */
-function responsivePlugin(steps?: number | number[], element?: HTMLImageElement, responsiveImage?: CloudinaryImage, htmlPluginState?: HtmlPluginState, analyticsOptions?: AnalyticsOptions): Promise<void | string> | boolean {
+function responsivePlugin(steps?: number | number[], element?:HTMLImageElement, responsiveImage?: CloudinaryImage, htmlPluginState?: HtmlPluginState, analyticsOptions?: AnalyticsOptions): Promise<void | string> | boolean {
 
-    if (!isBrowser()) return true;
+    if(!isBrowser()) return true;
 
-    if (!isImage(element)) return;
+    if(!isImage(element)) return;
 
-    return new Promise((resolve) => {
-        htmlPluginState.cleanupCallbacks.push(() => {
+    return new Promise((resolve)=>{
+        htmlPluginState.cleanupCallbacks.push(()=>{
             window.removeEventListener("resize", resizeRef);
             resolve('canceled');
         });
@@ -47,8 +47,8 @@ function responsivePlugin(steps?: number | number[], element?: HTMLImageElement,
         onResize(steps, element, responsiveImage, analyticsOptions);
 
         let resizeRef: any;
-        htmlPluginState.pluginEventSubscription.push(() => {
-            window.addEventListener('resize', resizeRef = debounce(() => {
+        htmlPluginState.pluginEventSubscription.push(()=>{
+            window.addEventListener('resize', resizeRef = debounce(()=>{
                 onResize(steps, element, responsiveImage, analyticsOptions);
             }, 100));
         });
@@ -64,7 +64,7 @@ function responsivePlugin(steps?: number | number[], element?: HTMLImageElement,
  * @param responsiveImage {CloudinaryImage}
  * @param analyticsOptions {AnalyticsOptions} analytics options for the url to be created
  */
-function onResize(steps?: number | number[], element?: HTMLImageElement, responsiveImage?: CloudinaryImage, analyticsOptions?: AnalyticsOptions) {
+function onResize(steps?: number | number[], element?:HTMLImageElement, responsiveImage?: CloudinaryImage, analyticsOptions?: AnalyticsOptions){
     updateByContainerWidth(steps, element, responsiveImage);
     element.src = responsiveImage.toURL(getAnalyticsOptions(analyticsOptions));
 }
@@ -76,24 +76,24 @@ function onResize(steps?: number | number[], element?: HTMLImageElement, respons
  * @param element {HTMLImageElement} The image element
  * @param responsiveImage {CloudinaryImage}
  */
-function updateByContainerWidth(steps?: number | number[], element?: HTMLImageElement, responsiveImage?: CloudinaryImage) {
+function updateByContainerWidth(steps?: number | number[], element?:HTMLImageElement, responsiveImage?: CloudinaryImage){
     // Default value for responsiveImgWidth, used when no steps are passed.
     let responsiveImgWidth = element.parentElement.clientWidth;
 
-    if (isNum(steps)) {
+    if(isNum(steps)){
         const WIDTH_INTERVALS = steps as number;
         // We need to force the container width to be intervals of max width.
-        responsiveImgWidth = Math.ceil(responsiveImgWidth / WIDTH_INTERVALS) * WIDTH_INTERVALS;
+        responsiveImgWidth = Math.ceil(responsiveImgWidth / WIDTH_INTERVALS ) * WIDTH_INTERVALS;
 
-    } else if (Array.isArray(steps)) {
-        responsiveImgWidth = steps.reduce((prev, curr) => {
+    } else if(Array.isArray(steps)){
+        responsiveImgWidth = steps.reduce((prev, curr) =>{
             return (Math.abs(curr - responsiveImgWidth) < Math.abs(prev - responsiveImgWidth) ? curr : prev);
         });
     }
 
     responsiveImage.transformation.actions.forEach((action, index) => {
         if (action instanceof Action && action.getActionTag() === 'responsive') {
-            responsiveImage.transformation.actions[index] = scale(responsiveImgWidth).setActionTag('responsive');
+            responsiveImage.transformation.actions[index]  = scale(responsiveImgWidth).setActionTag('responsive');
         }
     });
 }
