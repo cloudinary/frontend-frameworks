cdfec57a65088a49366f656250fbea7f54d92dc1
Lior Ben-David <lior.ben-david@cloudinary.com>
2023-01-29 11:00:29 +0200

    test: use fixed analytics options. remove html video layer tests

--- packages/html/__tests__/HtmlImageLayer.test.ts
+++ packages/html/__tests__/HtmlImageLayer.test.ts
@@ -1,14 +1,21 @@
 import {CloudinaryImage} from "@cloudinary/url-gen";
 import {HtmlImageLayer} from "../src";
+import {AnalyticsOptions} from "../src/types";
+
+const sdkAnalyticsTokens:AnalyticsOptions = {
+    sdkSemver: '1.0.0',
+    techVersion: '1.0.0',
+    sdkCode: 'X'
+}
 
 describe('HtmlImageLayer tests', function () {
     const cldImage = new CloudinaryImage('sample', {cloudName: 'demo'});
 
     it('should set image src', function () {
         const img = document.createElement('img');
-        new HtmlImageLayer(img, cldImage, []);
+        new HtmlImageLayer(img, cldImage, [], sdkAnalyticsTokens);
         process.nextTick(() => {
-            expect(img.src).toEqual('https://res.cloudinary.com/demo/image/upload/sample?_a=ATRSRAA0')
+            expect(img.src).toEqual('https://res.cloudinary.com/demo/image/upload/sample?_a=AXAABAB0')
         })
     });
 });
