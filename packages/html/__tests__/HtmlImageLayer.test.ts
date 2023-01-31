a26f446fc135308227556b001deb20ce099bac3d
Lior Ben-David <lior.ben-david@cloudinary.com>
2023-01-31 14:22:20 +0200

    test: add tests for feature flags analytics tokens

--- packages/html/__tests__/HtmlImageLayer.test.ts
+++ packages/html/__tests__/HtmlImageLayer.test.ts
@@ -1,11 +1,22 @@
 import {CloudinaryImage} from "@cloudinary/url-gen";
 import {HtmlImageLayer} from "../src";
 import {AnalyticsOptions} from "../src/types";
+import {responsive} from "../src/plugins/responsive";
+import {placeholder} from "../src/plugins/placeholder";
+import {lazyload} from "../src/plugins/lazyload";
+import {accessibility} from "../src/plugins/accessibility";
 
-const sdkAnalyticsTokens:AnalyticsOptions = {
+jest.useFakeTimers();
+
+const sdkAnalyticsTokens: AnalyticsOptions = {
     sdkSemver: '1.0.0',
     techVersion: '1.0.0',
     sdkCode: 'X'
+}
+
+const flushPromises = async () => {
+    jest.advanceTimersByTime(100);
+    await Promise.resolve();
 }
 
 describe('HtmlImageLayer tests', function () {
@@ -14,8 +25,32 @@ describe('HtmlImageLayer tests', function () {
     it('should set image src', function () {
         const img = document.createElement('img');
         new HtmlImageLayer(img, cldImage, [], sdkAnalyticsTokens);
+        jest.advanceTimersByTime(100);
         process.nextTick(() => {
-            expect(img.src).toEqual('https://res.cloudinary.com/demo/image/upload/sample?_a=AXAABAB0')
+            expect(img.src).toEqualAnalyticsToken('AXAABAB0');
         })
+    });
+
+    it('should use feature flag for responsive plugin', async function () {
+        const parentElement = document.createElement('div');
+        const img = document.createElement('img');
+        parentElement.append(img);
+        new HtmlImageLayer(img, cldImage, [responsive({steps: [100]})], sdkAnalyticsTokens);
+        await flushPromises();
+        expect(img.src).toEqualAnalyticsToken('AXAABABA');
+    });
+
+    it('should use feature flag for placeholder plugin', async function () {
+        const img = document.createElement('img');
+        new HtmlImageLayer(img, cldImage, [placeholder()], sdkAnalyticsTokens);
+        await flushPromises();
+        expect(img.src).toEqualAnalyticsToken('AXAABABB');
+    });
+
+    it('should use feature flag for accessibility plugin', async function () {
+        const img = document.createElement('img');
+        new HtmlImageLayer(img, cldImage, [accessibility()], sdkAnalyticsTokens);
+        await flushPromises();
+        expect(img.src).toEqualAnalyticsToken('AXAABABD');
     });
 });
