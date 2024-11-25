2724cba7d8e618a051754ae1e506fa5f0bee6451
jerzy-mankowski <jerzy.mankowski@cloudinary.com>
2024-11-25 11:45:33 +0100

    feat: mocha setup

--- packages/react/setup.jest.ts
+++
@@ -1,5 +0,0 @@
-const { TextDecoder, TextEncoder, ReadableStream } = require('node:util');
-
-globalThis.TextDecoder = TextDecoder;
-globalThis.TextEncoder = TextEncoder;
-globalThis.ReadableStream = ReadableStream;
