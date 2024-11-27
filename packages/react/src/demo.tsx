2653952d2f66ac19514f3493b31db360d2db9003
jerzy-mankowski <jerzy.mankowski@cloudinary.com>
2024-11-27 13:52:32 +0100

    chore: typecheck adjustments

--- packages/react/src/demo.tsx
+++ packages/react/src/demo.tsx
@@ -11,7 +11,7 @@ const NewComponentUsage = () => {
 };
 
 const Demo = () => {
-  return <CloudinaryImg />;
+  return <>icant</>;
 };
 
 document.addEventListener('DOMContentLoaded', () => {
--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,3 +1,3 @@
 export { CloudinaryImg } from './AdvancedImage';
-export { AdvancedVideo } from './AdvancedVideo';
+// export { AdvancedVideo } from './AdvancedVideo';
 
