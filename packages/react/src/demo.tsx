48df3be809ffccd992392d828f794278bccf1642
jerzy-mankowski <jerzy.mankowski@cloudinary.com>
2024-11-27 18:54:27 +0100

    feat: basic demo setup

--- packages/react/src/demo.tsx
+++ packages/react/src/demo.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import { Cloudinary } from '@cloudinary/url-gen';
+import { scale } from '@cloudinary/url-gen/actions/resize';
+import { sepia, backgroundRemoval } from '@cloudinary/url-gen/actions/effect';
+import { format, quality } from '@cloudinary/url-gen/actions/delivery';
 import { CloudinaryImg } from './index';
-import { ResizeSimpleAction } from '@cloudinary/url-gen/actions/resize/ResizeSimpleAction';
 
 const LegacyComponentUsage = () => {
-  const cloudinary = new Cloudinary();
-  const cloudinaryImageObject = cloudinary.image('sample')
-    .resize(new ResizeSimpleAction('pixelated', 200));
+  const cloudinary = new Cloudinary({
+    cloud: {
+      cloudName: 'demo'
+    }
+  });
+  const cloudinaryImageObject = cloudinary
+    .image('front_face')
+    .effect(backgroundRemoval())
+    .backgroundColor('lightblue')
+    .effect(sepia())
+    .resize(scale().height(333))
+    .delivery(format('auto'))
+    .delivery(quality('auto'));
 
   return (
     <>
@@ -18,28 +30,37 @@ const LegacyComponentUsage = () => {
     </>
   );
 };
+// https://res.cloudinary.com/dzi6ifrzp/image/upload/Proboscis_Monkey_%28Nasalis_larvatus%29_male_%286707344031%29_jahicx.jpg
+// https://asset.cloudinary.com/dzi6ifrzp/2de2f0efe1c963abdd612eb6a6714cf5
 
-const NewComponentUsage = () =>
-  (
-    <>
-      <div>New Component Usage</div>
-      <div>
-        <CloudinaryImg
-          src='https://res.cloudinary.com/dzi6ifrzp/image/upload/Proboscis_Monkey_%28Nasalis_larvatus%29_male_%286707344031%29_jahicx.jpg'
-          alt='proboscis monkey'
-          height={200}
-        />
-      </div>
-    </>
-  );
+const NewComponentUsage = () => (
+  <>
+    <div>New Component Usage</div>
+    <div>
+      <CloudinaryImg
+        cloudName='demo'
+        src='https://res.cloudinary.com/demo/image/upload/front_face?_a=DATAg1AAZAA0'
+        alt='proboscis monkey'
+        removeBackground
+        format='auto'
+        quality='auto'
+        background={{ type: 'color', color: 'lightblue' }}
+        height={333}
+        effects={[{ type: 'sepia' }]}
+      />
+    </div>
+  </>
+);
 
 const Demo = () => {
-  return <>
-    <LegacyComponentUsage />
-  </>;
+  return (
+    <>
+      <LegacyComponentUsage />
+      <NewComponentUsage />
+    </>
+  );
 };
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDom.render(<Demo />, document.getElementById('root'));
 });
-
