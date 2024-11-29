9c41a0771eef11289a2c38bc5dbf5791ea10e6d5
jerzy-mankowski <jerzy.mankowski@cloudinary.com>
2024-11-29 12:57:02 +0100

    feat: initial gravity support

--- packages/react/src/transformationParsers/parseGravity.ts
+++ packages/react/src/transformationParsers/parseGravity.ts
@@ -1,11 +1,43 @@
 import { Gravity } from '../transformationTypes/gravity';
 
+type SimpleGravity = Gravity extends infer GravityValue ? GravityValue extends string ? GravityValue : never : never;
+
 // FIXME fill in missing gravity parsing
 export const parseGravity = (gravity: Gravity) => {
-  switch (gravity) {
+  const camelCaseToSnakeCase = <Str extends string>(str: Str) => str.replace(/([A-Z]{1})/g, (value) => `_${value.toLowerCase()}`);
+  const simpleGravityParser = (gravity: SimpleGravity) => `g_${camelCaseToSnakeCase(gravity)}`;
+
+  const gravityToUrlComponent = {
+    auto: simpleGravityParser,
+    center: simpleGravityParser,
+    north: simpleGravityParser,
+    northWest: simpleGravityParser,
+    northEast: simpleGravityParser,
+    east: simpleGravityParser,
+    south: simpleGravityParser,
+    southWest: simpleGravityParser,
+    southEast: simpleGravityParser,
+    west: simpleGravityParser
+  } satisfies Record<Gravity, <G extends SimpleGravity>(gravity: G) => `g_${string}`>;
+
+  if (typeof gravity === 'string') {
+    return gravityToUrlComponent[gravity](gravity);
+  }
+
+  switch (gravity.mode) {
+    case 'special':
+      return '';
+    case 'object':
+      return '';
     case 'auto':
+      return '';
+    case 'clippingPath':
+      return '';
+    case 'region':
+      return '';
+    case 'trackPerson':
       return '';
   }
 
-  return '';
+  return 'g_auto';
 };
