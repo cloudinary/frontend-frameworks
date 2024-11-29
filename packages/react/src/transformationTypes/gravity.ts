db0bb0650a1e7d1f7d63e5a14d57a8e5731d573b
jerzy-mankowski <jerzy.mankowski@cloudinary.com>
2024-11-29 12:57:02 +0100

    feat: initial gravity support

--- packages/react/src/transformationTypes/gravity.ts
+++ packages/react/src/transformationTypes/gravity.ts
@@ -1,26 +1,78 @@
-// FIXME add custom gravity support
-export type Gravity =
+type CompassPosition =
   | 'north'
-  | 'north_west'
-  | 'north_east'
+  | 'northWest'
+  | 'northEast'
   | 'south'
-  | 'south_west'
-  | 'south_east'
+  | 'southWest'
+  | 'southEast'
   | 'west'
   | 'east'
-  | 'center'
-  | 'face'
-  | 'faces'
-  | 'auto'
-  | `auto:${string}`
-  | 'adv_face'
-  | 'adv_faces'
-  | 'custom'
-  | 'custom:face'
-  | 'custom:faces'
-  | 'custom:auto'
-  | 'custom:adv_face'
-  | 'custom:adv_faces'
-  | 'ocr_text'
-  | 'body'
-  | 'liquid';
+  | 'center';
+
+type DetectionAlgorithm = 'face' | 'faces' | 'advancedFace' | 'advancedFaces' | 'advancedEyes';
+
+type SpecialPosition = { mode: 'special'; } & ({
+  position: 'decomposeTile' | 'liquid' | 'ocrText';
+} | {
+  position: 'custom';
+  positionFallback?: DetectionAlgorithm;
+} | {
+  position: DetectionAlgorithm;
+  positionFallback?: CompassPosition;
+} | {
+  position: 'xy';
+  x: number;
+  y: number;
+});
+
+type Object = {
+  mode: 'object';
+  focus: string;
+}
+
+type Auto = 'auto' | {
+  mode: 'auto';
+  algorithm?: 'subject' | 'classic';
+  /**
+   * @default faces
+   */
+  focus?: string | string[];
+  /**
+   * @description 0 - 100
+   */
+  thumbAggressiveness?: number;
+  /**
+   *@default on
+   */
+  ruleOfThirds?: 'on' | 'off';
+};
+
+type ClippingPath = {
+  mode: 'clippingPath',
+  focus: string;
+};
+
+type Region = {
+  mode: 'region';
+  focus: string;
+  height: number;
+  width: number;
+};
+
+type TrackPerson = {
+  mode: 'trackPerson';
+  focus: string;
+  /**
+   * @default east
+   */
+  position?: CompassPosition;
+};
+
+export type Gravity =
+  | CompassPosition
+  | ClippingPath
+  | Region
+  | TrackPerson
+  | Auto
+  | SpecialPosition
+  | Object;
