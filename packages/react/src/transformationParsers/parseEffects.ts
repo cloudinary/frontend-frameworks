6aea94e2ceaa8c953aba5fa325a16748dc1d301b
jerzy-mankowski <jerzy.mankowski@cloudinary.com>
2024-12-02 13:30:21 +0100

    feat: added gamma, grayscale and light effect support

--- packages/react/src/transformationParsers/parseEffects.ts
+++ packages/react/src/transformationParsers/parseEffects.ts
@@ -10,6 +10,12 @@ export const parseEffects = (effects: Effect[]): string => {
           return effect.mode === 'fineEdges' ? 'e_background_removal:fineedges_y' : 'e_background_removal';
         case 'fade':
           return `e_fade:${effect.duration}`;
+        case 'gamma':
+          return `e_gamma:${effect.duration}`;
+        case 'grayscale':
+          return 'e_grayscale';
+        case 'light':
+          return `e_light${effect.intensity ? `:shadowintensity_${effect.intensity}` : ''}`;
       }
     })
     .join('');
