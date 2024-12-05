a0bdbccf9d1a0831e1d9814cc02be4f25870e027
jerzy-mankowski <jerzy.mankowski@cloudinary.com>
2024-12-05 17:49:57 +0100

    chore: cleanup

--- packages/react/src/transformationParsers/parseHeight.ts
+++ packages/react/src/transformationParsers/parseHeight.ts
@@ -1,4 +1,3 @@
 import { HeightOption } from '../transformationTypes/resize';
 
-// FIXME move it inside of parseResize
 export const parseHeight = (height: HeightOption) => `h_${height}`;
--- packages/react/src/transformationParsers/parseWidth.ts
+++ packages/react/src/transformationParsers/parseWidth.ts
@@ -1,4 +1,3 @@
 import { WidthOption } from '../transformationTypes/resize';
 
-// FIXME move it inside of parseResize
 export const parseWidth = (width: WidthOption) => `w_${width}`;
